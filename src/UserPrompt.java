7f87fbd438f2cad08777b02a05f9232e87c39076
styxq <pawelstyczen@outlook.com>
2018-07-31 00:48:06 +0200

    adding some changes

---
+++ src/UserPrompt.java
@@ -0,0 +1,25 @@
+public class UserPrompt {
+    private int a;
+    private int b;
+    private int c;
+
+    public UserPrompt(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+
+    public int getA() {
+        return this.a;
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    public int getC() {
+        return this.c;
+    }
+}
--- src/com/styxq/Main.java
+++ src/com/styxq/Main.java
@@ -3,6 +3,17 @@ package com.styxq;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       
+
+
+        System.out.println();
+
     }
+
+    public int result(int a, int b){
+
+
+        return(a + b);
+    }
+
 }
